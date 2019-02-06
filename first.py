e96884a36f737aa62ace164bf845e7bfe84c7035
anujkumar98 <anujcs2020@gmail.com>
2019-02-07 00:17:50 +0530

    Day2 learning continues..

--- first.py
+++ first.py
@@ -1,5 +1,10 @@
+#importing openCv
 import cv2
+#opening image in pwd using imread
 image=cv2.imread('cat.png')
+#changing the format of the open image and saving a new copy
 cv2.imwrite('edited_pic.jpg',image)
+#opening image in grayscale format
 edited_grey=cv2.imread('edited_pic.jpg',cv2.IMREAD_GRAYSCALE)
+#saving the grey scale image.
 cv2.imwrite('grey_cat.jpg',edited_grey)
---
+++ image_from_numpy.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  6 23:24:30 2019
+
+@author: Anuj Kumar
+"""
+#importing opencv
+import cv2
+#importing numpy
+import numpy as np
+#creating a numpy array
+img=np.zeros((3,3),dtype=np.uint8)
+print(img)
+#converting it to BGR format.
+img=cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+print(img)
